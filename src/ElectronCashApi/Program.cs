4946e4e699800a5d433c3704324bba91f7a4813a
Shawn Shaddock <shawnshaddock@gmail.com>
2018-05-01 01:25:30 -0400

    Changed listening address to all addresses.

--- src/ElectronCashApi/Program.cs
+++ src/ElectronCashApi/Program.cs
@@ -8,7 +8,7 @@ namespace ElectronCashApi
         static void Main(string[] args)
         {
             JsonRpc.RegisterMethods(typeof(Program).Assembly);
-            JsonRpc.Start("http://127.0.0.1:80");
+            JsonRpc.Start("http://0.0.0.0:80");
 
             Console.ReadLine();
         }
